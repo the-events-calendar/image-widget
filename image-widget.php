0c1bf58071fd239b378e1265236aedc43889d1b2
Zach Tirrell <zbtirrell@gmail.com>
2017-02-10 13:26:39 -0500

    removing unneeded screenshots and tweaking a bit of wording

--- image-widget.php
+++ image-widget.php
@@ -423,7 +423,7 @@ class Tribe_Image_Widget extends WP_Widget {
 		$version_key = '_image_widget_version';
 		if ( get_site_option( $version_key ) == self::VERSION ) return;
 
-		$msg = sprintf( __( 'Thanks for upgrading the Image Widget! If you like this plugin, please consider <a href="%s" target="_blank">rating it</a> and maybe even check out our premium plugins including our <a href="%s" target="_blank">Events Calendar Pro</a>!', 'image-widget' ), 'http://wordpress.org/plugins/image-widget/?source=image-widget&pos=nag', 'https://theeventscalendar.com/product/wordpress-events-calendar-pro/?source=image-widget&pos=nag' );
+		$msg = sprintf( __( 'Thanks for using the Image Widget! If you like this plugin, please consider <a href="%s" target="_blank">rating it</a> and maybe even check out our premium plugins including our <a href="%s" target="_blank">Events Calendar Pro</a>!', 'image-widget' ), 'http://wordpress.org/plugins/image-widget/?source=image-widget&pos=nag', 'https://theeventscalendar.com/product/wordpress-events-calendar-pro/?source=image-widget&pos=nag' );
 		echo "<div class='update-nag'>$msg</div>";
 
 		update_site_option( $version_key, self::VERSION );
